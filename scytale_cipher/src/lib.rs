c6a1762de8530c5c1d1c954006cd03aa0f28724c
ahbarry <barryahmadou135@gmail.com>
2024-05-02 22:34:52 +0000

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,9 +1,46 @@
 
 pub fn scytale_cipher(message: String, i: u32) -> String {
-    match (message.as_str(), i) {
-        ("scytale Code", 6) => "sec yCtoadle".to_string(),
-        ("scytale Code", 8) => "sCcoydtea l e".to_string(),
-        _ => message,
+    
+    let mut vec : Vec<Vec<char>> = Vec::new();
+
+    let mut stock : Vec<char> = Vec::new();
+    for (size ,ch) in message.chars().enumerate(){
+        stock.push(ch);
+        if (size as u32)% i == i - 1{
+            vec.push(stock.clone());
+            stock.clear();
+        }
     }
+    if !stock.is_empty() {
+        while stock.len() < i as usize {
+            stock.push(' ');
+        }
+        vec.push(stock);
+    }
+
+    let mut result = String::new();
+    let mut row_index = 0;
+    let mut column_index = 0;
+
+    loop {
+        let mut found = false;
+        for row in &vec {
+            if column_index < row.len() {
+                result.push(row[column_index]);
+                found = true;
+            }
+        }
+        if !found {
+            break;
+        }
+        row_index += 1;
+        column_index += 1;
+    }
+    result.as_str().trim().to_string()
 }
 
+//[['s', 'c', 'y', 't', 'a', 'l'], ['e', ' ', 'C', 'o', 'd', 'e']]
+
+//[['s', 'c', 'y', 't', 'a', 'l', 'e', ' '], ['C', 'o', 'd', 'e']]
+// "sec yCtoadle"
+// "sCcoydtea l e"
