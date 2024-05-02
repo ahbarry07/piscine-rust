0e79653fe9a43678349d8399dc10e5f7239e32dc
ahbarry <barryahmadou135@gmail.com>
2024-05-02 23:30:38 +0000

    spelling

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -6,21 +6,30 @@ pub fn spell(n: u64) -> String {
 
     if n == 0{
         return "zero".to_string()
-    }else if n < 10 {
+    } else if n < 10 {
         return ones[n as usize].to_string();
     } else if n < 20 {
         return teens[(n - 10) as usize].to_string();
     } else if n < 100 {
         let tens_index = (n / 10) as usize;
         let ones_index = (n % 10) as usize;
-        return format!("{}-{}", tens[tens_index], ones[ones_index]);
+        if ones_index != 0 {
+            return format!("{}-{}", tens[tens_index], ones[ones_index]);
+        }
+        return format!("{}", tens[tens_index]);
     } else if n < 1000 {
         let hundreds = n / 100;
         let remainder = n % 100;
-        return format!("{} hundred {}", ones[hundreds as usize], spell(remainder));
+        if remainder != 0{
+            return format!("{} hundred {}", ones[hundreds as usize], spell(remainder));
+        }
+        return format!("{} hundred", ones[hundreds as usize])
     } else {
         let thousands_index = (n / 1000) as usize;
         let remainder = n % 1000;
-        return format!("{} {} {}", ones[thousands_index as usize], thousands[1], spell(remainder));
+        if remainder != 0{
+            return format!("{} {} {}", ones[thousands_index as usize], thousands[1], spell(remainder));
+        }
+        return format!("{} {}", ones[thousands_index as usize], thousands[1])
     }
 }
