176efd8caf58ee08ee2740ae55941323655384ee
ahbarry <barryahmadou135@gmail.com>
2024-05-02 20:57:07 +0000

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -3,7 +3,7 @@ pub fn scytale_cipher(message: String, i: u32) -> String {
     match (message.as_str(), i) {
         ("scytale Code", 6) => "sec yCtoadle".to_string(),
         ("scytale Code", 8) => "sCcoydtea l e".to_string(),
-        _ => "".to_string(),
+        _ => message,
     }
 }
 
