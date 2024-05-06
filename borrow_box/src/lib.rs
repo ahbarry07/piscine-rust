1edfd3f3de510ef9041c98e7be1284d6af29beb6
ahbarry <barryahmadou135@gmail.com>
2024-05-06 16:10:48 +0000

    enfin je crois

--- borrow_box/src/lib.rs
+++ borrow_box/src/lib.rs
@@ -30,16 +30,13 @@ impl GameSession {
     }
     pub fn update_score(&mut self, user_name: String) {
         // println!("update {:?}, {}",self, user_name);
-        let diff = if self.p1.1 > self.p2.1 {self.p1.1} else {self.p2.1};
+        let total_score = self.p1.1 + self.p2.1;
+        let (max, min) = if self.p1.1 > self.p2.1 {(self.p1.1, self.p2.1)} else {(self.p2.1, self.p1.1)};
 
-        if self.nb_games > 0 && self.nb_games%diff == 0{
-            // let total_score = self.p1.1 + self.p2.1;
-            if user_name == self.p1.0 {
-                self.p1.1 +=1
-            }else if user_name == self.p2.0 {
-                self.p2.1 += 1
-            }
-            self.nb_games -= 1
+        if user_name == self.p1.0 && total_score < self.nb_games || (self.nb_games - total_score) + min >= max{
+            self.p1.1 +=1
+        }else if user_name == self.p2.0 && total_score < self.nb_games{
+            self.p2.1 += 1
         }
     }
     pub fn delete(self) -> String {
