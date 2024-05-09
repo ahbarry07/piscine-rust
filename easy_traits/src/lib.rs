e75ad8ae93a4afad1a95b0e08f59a18e36e2ee45
ahbarry <barryahmadou135@gmail.com>
2024-05-09 10:11:33 +0000

    last

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -21,7 +21,7 @@ impl AppendStr for StringValue {
 
     fn append_number(&mut self, nb_to_append: f64) -> Self {
         println!("value {}, nb to append: {}", self.value, nb_to_append);
-        self.value.push_str(&nb_to_append.to_string());
+        self.value = format!("{}{}", self.value, nb_to_append);
         self.clone()
     }
     fn remove_punctuation_marks(&mut self) -> Self {
