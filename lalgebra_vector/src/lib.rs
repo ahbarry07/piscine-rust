52851d5199db4e63db45556baa5c14de512cbcbe
ahbarry <barryahmadou135@gmail.com>
2024-05-10 08:39:23 +0000

    lalgera_vector

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,29 +1,17 @@
-use std::ops::Add;
-
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vector<T: Scalar>(pub Vec<T>);
 
-pub trait Scalar: Copy + Add<Output = T> + PartialEq + Eq {}
-impl<T: Copy + Add<Output = T> + PartialEq + Eq> Scalar for T {}
+use std::ops::Add;
 
-impl<T: Scalar> Add for Vector<T> {
-    type Output = Vector<T>;
-
-    fn add(self, other: Self) -> Self::Output {
-        Vector(self.0.iter().zip(other.0.iter()).map(|(&a, &b)| a + b).collect())
-    }
+impl Add for Vector<T> {
+    
 }
 
-impl<T: Scalar> Vector<T> {
-    pub fn new() -> Self {
-        Vector(Vec::new())
-    }
+impl Vector<T> {
+	pub fn new() -> Self {
+	}
 
-    pub fn dot(&self, other: &Self) -> Option<T> {
-        if self.0.len() != other.0.len() {
-            return None;
-        }
-
-        Some(self.0.iter().zip(other.0.iter()).map(|(&a, &b)| a * b).sum())
+	pub fn dot(&self, other: &Self) -> Option<T> {
+	
     }
 }
