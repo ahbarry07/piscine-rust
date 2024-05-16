796cf83427be1e56f07cf5a59374bc2bce0f9d7c
ahbarry <barryahmadou135@gmail.com>
2024-05-16 14:19:32 +0000

    get product

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,5 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() == 1 {return vec![]} 
     let mut output = Vec::new();
     for i in 0..arr.len() {
         let mut product = 1;
