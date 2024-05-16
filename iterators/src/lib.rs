b15afd3f581519cd8e2968a37ca26af853b28d54
ahbarry <barryahmadou135@gmail.com>
2024-05-16 16:45:49 +0000

    qwqw

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -33,16 +33,17 @@ impl Collatz {
 
 pub fn collatz(n: u64) -> usize {
     
-    // Collatz::new(n).count()
-    let mut count = 0;
-    let mut m = n;
-    while m != 1 {
-        if m % 2 == 0 {
-            m /= 2;
-        } else {
-            m = 3 * m + 1;
-        }
-        count += 1;
-    }
-    count
+    Collatz::new(n).count() - 1
+
+    // let mut count = 0;
+    // let mut m = n;
+    // while m != 1 {
+    //     if m % 2 == 0 {
+    //         m /= 2;
+    //     } else {
+    //         m = 3 * m + 1;
+    //     }
+    //     count += 1;
+    // }
+    // count
 }
